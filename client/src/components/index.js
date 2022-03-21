c69b1d2a74984d23c21d51286ce629f938b6a1bb
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-03-21 11:35:12 +0100

    clean up unused files

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,5 @@
 import Header from "./Header";
 import Navbar from "./Navbar";
-import Item from "./Item";
-import ItemList from "./ItemList";
 import ProductCard from "./ProductCard";
 
-export { Navbar, Header, Item, ItemList, ProductCard };
+export { Navbar, Header, ProductCard };
