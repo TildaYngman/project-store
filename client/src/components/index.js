9ceb8cdfb71ba285e2e3e98b6bf7cd3165eb58f4
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-03-23 14:57:05 +0100

    added scss variables and changed all colors to the variables and created a Searchfield

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,6 @@
 import Header from "./Header";
 import Navbar from "./Navbar";
 import ProductCard from "./ProductCard";
+import SearchField from "./SearchField/SearchField";
 
-export { Navbar, Header, ProductCard };
+export { Navbar, Header, ProductCard, SearchField };
