3401111fd545a4e88e09e3d8b4b3e2d653debb51
Tilda Yngman <Matilda.yngman@hyperisland.se>
2022-05-29 13:04:10 +0200

    fixed bug where everything went back to original values

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -1,13 +1,34 @@
-function Cart() {
+function Cart({ cartItems, setCartItems }) {
+  const totalPrice = cartItems.reduce(
+    (sum, obj) => obj.price * obj.quantity + sum,
+    0
+  );
+
   return (
     <section id="cart-container" className="hidden">
       <div className="full-overlay">
-        <div className="cart-item">
-          <h1>Your shopping cart</h1>
-        </div>
+        <h1>Your shopping cart!</h1>
       </div>
     </section>
   );
 }
+
+// const cartContent = () => {
+//   const { onAdd, items } = props;
+//   return (
+//     <div className="grid-item">
+//       <img
+//         className="grid-item__image"
+//         src={props.image}
+//         alt="A pricture of the product"
+//       />
+//       <h3 className="grid-item__info">
+//         {props.title ? props.title : "Coming soon"}
+//       </h3>
+//       <p className="grid-item__info">{props.price ? props.price : ""} SEK</p>
+//       <div className="grid-item__btn-container"></div>
+//     </div>
+//   );
+// };
 
 export default Cart;
